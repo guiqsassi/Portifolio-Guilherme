e884abf7f145b421692051a5e1c46eff102eb8d6
guiqsassi <guiqsassi@gmail.com>
2023-08-05 00:12:36 -0300

    final

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -38,7 +38,7 @@ function Projects(){
                 <h2>Projetos</h2>
             </div>
             <div className="projetos">
-            <ProjectView name="Lorax" linkExec="https://lorax-landing-page.vercel.app/" linkGit="https://github.com/guiqsassi/Lorax_landing_page" techs={["React","Css", "Javascript"]} date="26/05/2023" description="Este projeto é uma Landing page dedicada ao filme Lorax O Ladrão de florestas" image={Lorax}></ProjectView>
+            <ProjectView name="Lorax" linkExec="https://lorax-landing-page.vercel.app/" linkGit="https://github.com/guiqsassi/Lorax_landing_page" techs={["React","Css", "Javascript"]} date="26/05/2023" description="Este projeto é uma Landing page dedicada ao filme Lorax" image={Lorax}></ProjectView>
             <ProjectView name="PokeDb" date="28/06/2023" linkGit="https://github.com/guiqsassi/Pokedex" linkExec={"https://pokedb-six.vercel.app/"} image={Poke} techs={["React","Css", "Javascript"]} description="Projeto realizado utilizando a api de pokedex"></ProjectView>
             <ProjectView name="Calculadora React" date="23/05/2023" linkGit="https://github.com/guiqsassi/Calculator" linkExec={"https://calculator-swart-nine.vercel.app/"} image={Calculator} techs={["React","Css", "Javascript"]} description="Este projeto é uma Calculador feita em React"></ProjectView>
             </div> 
