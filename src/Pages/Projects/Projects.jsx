e475a3e26b4975af2384c73e4767b74de5330306
guiqsassi <guiqsassi@gmail.com>
2023-08-10 14:38:10 -0300

    new project

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import "./Projects.css"
 import Lorax from "../../images/Lorax.png"
 import Poke from "../../images/PokeDbPrint.png"
 import Calculator from "../../images/Calculator.png"
+import casaVerde from "../../images/casa_verde.png"
 
 // import Lorax from "../../images/Lorax.png"
 import ProjectView from "../../Components/ProjectView/ProjectView";
@@ -39,6 +40,7 @@ function Projects(){
             </div>
             <div className="projetos">
             <ProjectView name="Lorax" linkExec="https://lorax-landing-page.vercel.app/" linkGit="https://github.com/guiqsassi/Lorax_landing_page" techs={["React","Css", "Javascript"]} date="26/05/2023" description="Este projeto é uma Landing page dedicada ao filme Lorax" image={Lorax}></ProjectView>
+            <ProjectView name="Casa Verde" linkExec="https://7-days-alura-day1-el4m98t1m-guiqsassi.vercel.app/" linkGit="https://github.com/guiqsassi/7DaysAlura-day1.git" techs={["React","Css", "Javascript"]} date="26/05/2023" description="Este projeto é uma landing page criada a partir de um projeto figma durante o desafio 7 days of code da alura" image={Lorax}></ProjectView>
             <ProjectView name="PokeDb" date="28/06/2023" linkGit="https://github.com/guiqsassi/Pokedex" linkExec={"https://pokedb-six.vercel.app/"} image={Poke} techs={["React","Css", "Javascript"]} description="Projeto realizado utilizando a api de pokedex"></ProjectView>
             <ProjectView name="Calculadora React" date="23/05/2023" linkGit="https://github.com/guiqsassi/Calculator" linkExec={"https://calculator-swart-nine.vercel.app/"} image={Calculator} techs={["React","Css", "Javascript"]} description="Este projeto é uma Calculador feita em React"></ProjectView>
             </div> 
