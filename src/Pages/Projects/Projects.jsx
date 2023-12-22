4f48bf89906a8da754a771ad2b69e09dce6ee537
guiqsassi <guiqsassi@gmail.com>
2023-12-22 11:19:24 -0300

    chihiro

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -3,6 +3,7 @@ import Lorax from "../../images/Lorax.png"
 import Poke from "../../images/PokeDbPrint.png"
 import Calculator from "../../images/Calculator.png"
 import casaVerde from "../../images/casa_verde.png"
+import Chihiro from "../../images/chihiro.png"
 
 // import Lorax from "../../images/Lorax.png"
 import ProjectView from "../../Components/ProjectView/ProjectView";
@@ -39,6 +40,7 @@ function Projects(){
                 <h2>Projetos</h2>
             </div>
             <div className="projetos">
+            <ProjectView name="Chihiro-Lp" linkExec="https://chihiro-lp.vercel.app/" linkGit="https://github.com/guiqsassi/Chihiro-lp" techs={["React","Css", "Javascript"]} date="19/12/2023" description="Este projeto é uma Landing page dedicada para o filme A Viagem de Chihiro, feito a partir de um projeto figma da Codelândia" image={Chihiro}></ProjectView>
             <ProjectView name="Lorax" linkExec="https://lorax-landing-page.vercel.app/" linkGit="https://github.com/guiqsassi/Lorax_landing_page" techs={["React","Css", "Javascript"]} date="26/05/2023" description="Este projeto é uma Landing page dedicada ao filme Lorax" image={Lorax}></ProjectView>
             <ProjectView name="Casa Verde" linkExec="https://7-days-alura-day1-gz1sx0hvq-guiqsassi.vercel.app/" linkGit="https://github.com/guiqsassi/7DaysAlura-day1.git" techs={["React","Css", "Javascript"]} date="09/08/2023" description="Este projeto é uma landing page criada a partir de um projeto figma durante o desafio 7 days of code da alura" image={casaVerde}></ProjectView>
             <ProjectView name="PokeDb" date="28/06/2023" linkGit="https://github.com/guiqsassi/Pokedex" linkExec={"https://pokedb-six.vercel.app/"} image={Poke} techs={["React","Css", "Javascript"]} description="Projeto realizado utilizando a api de pokedex"></ProjectView>
